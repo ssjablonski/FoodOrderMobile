5f1e945eec89602cf4944f704176353aa1b8e600
Sebastian Jablonski <sebaa1080@gmail.com>
2024-08-03 20:35:07 +0200

    Added checkout button + total price in cart

--- src/providers/CartProvider.tsx
+++ src/providers/CartProvider.tsx
@@ -5,17 +5,20 @@ import { randomUUID } from "expo-crypto";
 type CartType = {
     items: CartItem[],
     addItem: (product: Product, size: CartItem['size']) => void,
-    updateQuantity: (itemId: string, amount: -1 | 1) => void
+    updateQuantity: (itemId: string, amount: -1 | 1) => void,
+    total: number
 }
 
 export const CartContext = createContext<CartType>({
     items: [],
     addItem: () => {},
-    updateQuantity: () => {}
+    updateQuantity: () => {},
+    total: 0
 })
 
 const CartProvider = ({ children }: PropsWithChildren) => {
     const [items, setItems] = useState<CartItem[]>([])
+
 
     const addItem = (product: Product, size: CartItem['size']) => {
         const existingItem = items.find(item => item.product === product && item.size === size)
@@ -42,8 +45,10 @@ const CartProvider = ({ children }: PropsWithChildren) => {
         }).filter((item) => item.quantity > 0))
     }
 
+    const total = items.reduce((acc, item) => acc + item.product.price * item.quantity, 0)
+
     return (
-        <CartContext.Provider value={{items, addItem, updateQuantity}}>
+        <CartContext.Provider value={{items, addItem, updateQuantity, total}}>
             {children}
         </CartContext.Provider>
     )
