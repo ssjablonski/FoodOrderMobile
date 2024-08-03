6358e6c7b0ae3408a6be908aa2ba39f9d201bae0
Sebastian Jablonski <sebaa1080@gmail.com>
2024-08-03 12:58:51 +0200

    Added types + ProductListItem component

---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,42 @@
+import Colors from '@/src/constants/Colors';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Product } from '../types';
+
+export const defaultPizzaImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png'
+
+type ProductListItemProps = {
+    product: Product;
+}
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
+  return (
+    <View style={styles.container}>
+      <Image source={{uri: product.image || defaultPizzaImage }} style={styles.image}/>
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    </View>
+  );
+}
+
+export default ProductListItem;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 20
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: '600',
+    marginVertical: 10,
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: 'bold',
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1
+  }
+});
