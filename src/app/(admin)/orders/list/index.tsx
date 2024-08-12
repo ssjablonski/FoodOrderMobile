a89854470c7d4364cc173a58fa6dfbf246bb1500
Sebastian Jablonski <sebaa1080@gmail.com>
2024-08-12 16:58:44 +0200

    Real time data subscriptions

--- src/app/(admin)/orders/list/index.tsx
+++ src/app/(admin)/orders/list/index.tsx
@@ -1,11 +1,15 @@
 import { ActivityIndicator, FlatList, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import OrderListItem from '@/src/components/OrderListItem'
 import { useAdminOrderList } from '@/src/api/orders'
+import { supabase } from '@/src/lib/supabase'
+import { useQueryClient } from '@tanstack/react-query'
+import { useInsertOrderSubscription } from '@/src/api/orders/subscription'
 
 const OrdersList = () => {
 
   const {data: orders, isLoading, error} = useAdminOrderList({archived: false})
+  useInsertOrderSubscription()
 
   if (isLoading) {
     return <ActivityIndicator />
--- src/app/(user)/orders/[id].tsx
+++ src/app/(user)/orders/[id].tsx
@@ -4,12 +4,14 @@ import { Stack, useLocalSearchParams } from 'expo-router'
 import OrderListItem from '@/src/components/OrderListItem'
 import OrderItemListItem from '@/src/components/OrderItemListItem'
 import { useOrderDetails } from '@/src/api/orders'
+import { useUpdateOrderSubscription } from '@/src/api/orders/subscription'
 
 
 const OrderDetailScreen = () => {
   const { id: idString } = useLocalSearchParams()
   const id = parseFloat(typeof idString === 'string' ? idString : idString[0])
   const {data: order, isLoading, error} = useOrderDetails(id)
+  useUpdateOrderSubscription(id)
 
   if (!order) {
     return <Text>Order not found</Text>
